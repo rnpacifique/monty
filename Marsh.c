51bb7b32049f22c0b5f3c13a7ceffbc0c040c35b
rnpacifique <rpacifique12@gmail.com>
2023-11-16 10:27:01 -0800

    files that sustract and read lines

---
+++ Marsh.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * swap - Switches the positions of the 
+ * two topmost elements in the stack
+ * @stack: stack Pointer 
+ * @line_number: The line number of the executing line
+ *
+ * Return: void
+ */
+
+void swap(my_stack_t **stack, unsigned int line_number)
+{
+	my_stack_t *top = (*stack);
+	my_stack_t *following = top->next;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L %u: can't swap, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	top->next = following->next;
+	top->prev = following;
+	following->prev = NULL;
+	following->prev = top;
+	following->next->prev = top;
+	if (top == (*stack))
+	{
+		(*stack) = following;
+	}
+}
---
+++ managewhite_spaces.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * trim_whitespace - handle whitespaces from a string
+ * @str: Pointer 
+ *
+ * Return: Pointer to handle string
+ */
+char *trim_whitespace(char *str)
+{
+	char *end;
+
+	while (isspace((unsigned char)*str))
+		str++;
+
+	if (*str == 0)
+		return (str);
+
+	end = str + strlen(str) - 1;
+
+	while (end > str && isspace((unsigned char)*end))
+		end--;
+
+	end[1] = '\0';
+
+	return (str);
+}
